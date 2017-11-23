461a3608342113cc170571a71095c0a895c95552
JackRhodes <jack-8729@hotmail.co.uk>
2017-11-23 11:24:53 +0000

    Added initial models

---
+++ Degree Application/Models/AccountModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Degree_Application.Models
+{
+    public class AccountModel
+    {
+
+        public int AccountId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "Must be 8 or more characters"), MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Phone]
+        public long Mobile { get; set; }
+
+        [Required]
+        [RegularExpression(@"([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})",ErrorMessage = "Invalid UK Postcode")]
+        public string PostCode { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+
+    }
+}
---
+++ Degree Application/Models/ItemModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Degree_Application.Models
+{
+    public class ItemModel
+    {
+
+        public int ItemId { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Please enter 50 characters or less"), MinLength(3)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Please enter 255 characters or less"), MinLength(1)]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Status { get; set; }
+
+        public byte[] Images { get; set; }
+
+    }
+}
